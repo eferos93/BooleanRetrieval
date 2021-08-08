844a6f0aca2de04a6e6bc46e59c62e34f82cb17c
eferos93 <eros.fabrici@gmail.com>
2021-08-08 17:25:58 +0200

    add Utils.normaliseText and Utils.tokenize

---
+++ src/main/scala/InvertedIndex.scala
@@ -0,0 +1,5 @@
+package org.information_retrieval.boolean_retrieval
+
+import scala.util.matching.Regex
+
+case class InvertedIndex()
--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -1,47 +1,21 @@
 package org.information_retrieval.boolean_retrieval
 
-import java.io.{File, FileInputStream, FileOutputStream}
-import java.util.zip.GZIPInputStream
 
 object Utils {
-//  def downloadData(folder: String, url: String): Unit = {
-//    import scala.language.postfixOps
-//    import java.net.URL
-//    import sys.process._
-//    new URL(url) #> new File(folder) !!
-//  }
-//
-//  val dataFolder: File = File("/data/")
-//  val url: String = "http://www.cs.cmu.edu/~ark/personas/data/MovieSummaries.tar.gz"
-//  println(java.nio.file.Files.notExists(new File("data").toPath))
-//  if (java.nio.file.Files.notExists(new File("data").toPath)) {
-//    println("data not found, downloading...")
-//    downloadData("data.tar.gz", url)
-//    val gzipInputStream: GZIPInputStream = new GZIPInputStream(new FileInputStream(new File("data.tar.gz")))
-//    val buffer: Array[Byte] = new Array[Byte](1024)
-//    val fileOutputStream: FileOutputStream = new FileOutputStream("data")
-//    var length = 0;
-//    while (length = gzipInputStream.read(buffer)) > 0 do
-//      length
-//      fileOutputStream.write(buffer, 0, )
+  def normaliseText(text: String): String = {
+//    pattern is:
+//    - ^\\w : not a word
+//    - ^\\s : not a  space
+//    - ^- : not a -
+//    .r.replaceAllIn(text, "")
+    text.replaceAll("[^\\w^\\s^-]", "").toLowerCase()
+  }
 
-    //    import java.io.{FileInputStream, FileOutputStream}
-    //    import java.util.zip.ZipInputStream
-    //    println("Data path not found, downloading...")
-    //    downloadData("data.tar.gz", url)
-    //    val fileInputStream = new FileInputStream("data.tar.gz")
-    //    val gZipInputStream = new GZIPInputStream(fileInputStream)
-    //    Stream.continually(gZipInputStream).takeWhile(_ != null).foreach {
-    //      file =>
-    //        val fileOutputStream = new FileOutputStream(file.getName)
-    //        val buffer = new Array[Byte](1024)
-    //        Stream.continually(gZipInputStream.read(buffer))
-    //          .takeWhile(_ != -1)
-    //          .foreach(fileOutputStream.write(buffer, 0, _))
-    //    }
+  def tokenize(movie: Movie): Array[String] = {
+    normaliseText(movie.description).split(" ")
+  }
 
+//  def main(args: Array[String]): Unit = {
+//    println(normaliseText("e.g., this is-a Test"))
 //  }
-  //  dataFolder.length() match {
-  //    case 0 => downloadData(dataFolder.getPath, url)
-  //  }
 }
