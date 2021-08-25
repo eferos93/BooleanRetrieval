44da9e8faf0d0aaf0b4ccc1db5577fa4321a5a1f
eferos93 <eros.fabrici@gmail.com>
2021-08-25 21:41:49 +0200

    started vsm

---
+++ src/main/scala/vector_space_model/VectorSpaceModel.scala
@@ -0,0 +1,6 @@
+package org.information_retrieval.boolean_retrieval
+package vector_space_model
+
+object VectorSpaceModel extends App {
+
+}
---
+++ src/main/scala/vector_space_model/package.scala
@@ -0,0 +1,45 @@
+package org.information_retrieval.boolean_retrieval
+
+import scala.io.Source
+
+package object vector_space_model {
+  val normaliseText: String => String = { text =>
+    //    replace everything that is not a letter or a space with empty string
+    text.replaceAll("[^a-zA-Z\\s]", "")
+  }
+
+//  def importDataSet() = {
+//    //    download it from: https://moodle2.units.it/mod/resource/view.php?id=195286
+//    val dataSetPath = "time/TIME.ALL"
+//    Source.fromFile(dataSetPath)
+//      .getLines()
+//      .withFilter(!_.isEmpty)
+////      .mkString("\n")
+////      .split("^\\*TEXT*$")
+////      .map(articleText => normaliseText(articleText).split(" "))
+////      .foreach { row =>
+////        if row.startsWith("*TEXT") then
+////
+////      }
+//  }
+
+//TODO: make it functional style
+  def importDataSet() = {
+    var articles: Array[Array[String]] = Array.empty
+    var temp: Array[String] = Array.empty
+    Source.fromFile("time/TIME.ALL")
+      .getLines()
+      .withFilter(!_.isEmpty)
+      .foreach { row =>
+        row.startsWith("*TEXT") match {
+          case true =>
+            if !temp.isEmpty then
+              articles = articles :+ temp
+              temp = Array.empty
+          case false =>
+            temp = temp :++ normaliseText(row).split(" ")
+        }
+      }
+    articles
+  }
+}
